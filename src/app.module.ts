89cbb5884318b82eaf253d2cddfdc2cce7077e9b
sravan kumar ganji <sravankumarganji@sravans-MacBook-Pro.local>
2020-07-27 11:21:17 -0400

    removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, TasksController],
-  providers: [AppService],
+  controllers: [TasksController],
+  providers: [],
 })
 export class AppModule {}
